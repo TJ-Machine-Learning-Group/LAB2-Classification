d9ea47eb85da1845bc1bc4d60f283490d170233c
UltraQ123 <1355211477@qq.com>
2022-05-07 14:24:06 +0800

    some Para

--- visualization.py
+++ visualization.py
@@ -1,40 +1,46 @@
+from traceback import TracebackException
 import seaborn as sns
 import matplotlib.pyplot as plt
 from typing import List
 import pandas as pd
 
-def getHeatMap(CM:List[List[int]],labels:List[any],path:str) ->None:
-    """
-    对于给定样本标签的混淆矩阵，生成热度图并存于给定的path下
-    """
-    #temp = list(reversed(labels))
-    temp =labels
-    result = pd.DataFrame(data=CM,index=temp,columns=labels,dtype=int)
-    print(result)
-    sns.set()
-    fig,ax = plt.subplots()
-    plt.rcParams['font.sans-serif'] = ['SimHei']
-    sns.heatmap(result, annot=True, ax=ax,fmt="d")  # 画热力图
-    ax.set_title('confusion matrix')  # 标题
-    ax.set_xlabel('predict')  # x 轴
-    ax.set_ylabel('true')  # y 轴
-    plt.savefig(path)
+class visual(object):
+    def __init__(self,CM:List[List[int]],labels:List[any]) -> None:
+        self.CM = pd.DataFrame(data=CM,index=labels,columns=labels,dtype=int)
+        self.labels = labels
+
+    def getHeatMap(self,path:str) -> None:
+        fig,ax = plt.subplots()
+        plt.rcParams['font.sans-serif'] = ['SimHei']
+        sns.heatmap(self.CM, annot=True, ax=ax,fmt="d")  # 画热力图
+        ax.set_title('confusion matrix')  # 标题
+        ax.set_xlabel('predict')  # x 轴
+        ax.set_ylabel('true')  # y 轴
+        plt.savefig(path)
+
+    def getPrecision(self,label:any)->str:
+        if label in self.labels:
+            print("the Precision of {} is {:.2f}".format(label,self.CM[label][label]/self.CM[label].sum()))
+        else:
+            raise ValueError("the label must in labels {}".format(self.labels))
+
+    def getRecall(self,label:any)->str:
+        if label in self.labels:
+            print("the Recall of {} is {:.2f}".format(label,self.CM[label][label]/self.CM.loc[label].sum()))
+        else:
+            raise ValueError("the label must in labels {}".format(self.labels))
+
+    def getAcc(self)->float:
+        return sum([self.CM[i][i] for i in self.labels])/self.CM.values.sum()
+
+
+
 
 
 if __name__ == "__main__":
-    CM = [
-[855,16,22,15,10,4,3,13,49,13],
-[6,918,2,5,2,1,2,1,16,47],
-[64,1,721,46,66,31,32,25,12,2],
-[20,5,32,668,44,129,35,39,11,17],
-[8,1,40,41,803,23,20,52,10,2],
-[7,0,22,117,27,753,10,55,4,5],
-[5,2,25,45,20,5,883,8,3,4],
-[8,2,4,14,25,23,2,917,1,4],
-[37,18,4,7,1,0,4,5,909,15],
-[19,45,1,8,3,1,6,5,20,892]
-]
-    # CM = None
-    # labels = ["飞机","汽车","鸟","猫","鹿","狗","蛙","马","羊","卡车"]
-    labels = None
-    getHeatMap(CM,labels,"test.png")
+    CM = [[626, 19, 87, 38, 48, 10, 10, 26, 87, 49], [23, 743, 9, 12, 7, 7, 11, 9, 45, 134], [52, 7, 567, 103, 113, 58, 35, 43, 9, 13], [19, 20, 61, 513, 97, 151, 60, 43, 14, 22],
+          [20, 5, 88, 58, 656, 34, 60, 68, 7, 4], [8, 2, 58, 236, 79, 490, 23, 84, 11, 9], [10, 10, 57, 69, 53, 27, 743, 12, 8, 11], [10, 7, 46, 56, 65, 61, 11, 727, 2, 15],
+          [61, 38, 25, 23, 17, 6, 9, 9, 764, 48], [25, 65, 16, 35, 12, 10, 21, 24, 32, 760]]
+    labels = ["飞机", "汽车", "鸟", "猫", "鹿", "狗", "蛙", "马", "羊", "卡车"]
+    vis = visual(CM,labels)
+    vis.getAcc()
